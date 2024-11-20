5cb102c8293fcd069b0dd4ef08fb0887fa6a6db9
aishanoor1439 <aisha.noor1439@gmail.com>
2024-11-20 10:34:17 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Total_amont_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalAmount = 0;
+            double price = 0;
+
+            //Initialization
+            int i = 1;
+
+            //Condition
+            while (true)
+            {
+                Console.WriteLine("Enter price of item {0} or enter a negative number to stop:", i);
+                price = Convert.ToDouble(Console.ReadLine());
+                
+                //Break the loop on negative value
+                if (price < 0)
+                {
+                    break;
+                }
+
+                totalAmount += price;
+
+                //Incrementation
+                i++;
+            }
+            Console.WriteLine("Total amount: {0}", totalAmount);
+        }
+    }
+}
